78e261ba2aad36f47494ba20f5c95a88b5105605
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2019-09-04 16:04:19 -0400

    fix(gitignore): remove .docz from repo

--- .docz/public/styles-1260dd1dfea2e707f943.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{357:function(n,w,o){}}]);
-//# sourceMappingURL=styles-1260dd1dfea2e707f943.js.map
--- .docz/public/styles-6cba3b3c385a7bfc18e4.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{357:function(n,w,o){}}]);
-//# sourceMappingURL=styles-6cba3b3c385a7bfc18e4.js.map
